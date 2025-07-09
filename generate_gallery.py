aafdbe5d699024ee973d2f10a70ba5227dc3b28d
RonDomario <zharkovroman1998@gmail.com>
2025-07-09 14:35:02 +0400

    Now displays the name of the current phase and updates every hour

--- generate_gallery.py
+++ generate_gallery.py
@@ -1,8 +1,13 @@
 from bot.utils.moon import Moonphase
 
-
 if __name__ == '__main__':
-    image = Moonphase().draw_img()
+    m = Moonphase()
+    image = m.draw_img()
     image.save("moonphase.png")
     gallery = Moonphase.make_gallery()
     gallery.save("gallery.png")
+    with open("README.md", "w") as file:
+        file.write(f"### Current Moon Phase: {m.__repr__()}\n"
+                   "![Moon Phase](moonphase.png)\n"
+                   "### Next Key Phases\n"
+                   "![Gallery](gallery.png)")
